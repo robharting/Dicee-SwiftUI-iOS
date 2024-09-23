1502fc283daaadc9ab1fa772ccad2ef81c4cdc69
Rob Harting <robharting@hotmail.com>
2024-09-23 07:56:45 +0200

    Initial Commit

---
+++ Dicee-SwiftUI-iOS/Dicee_SwiftUI_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUI_iOSApp.swift
+//  Dicee-SwiftUI-iOS
+//
+//  Created by Harting, R.P.G. (Rob) on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUI_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
